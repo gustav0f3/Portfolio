2f37e75cd70f0206f382362272820e388f286677
gustav0f3 <gustavo.fdh11@gmail.com>
2023-05-03 00:44:50 -0400

    Update Game.jsx

--- src/componentes/Game.jsx
+++ src/componentes/Game.jsx
@@ -5,21 +5,8 @@ function Game() {
   return (
     <section id={styles.fundo}>
       <h2 className={styles.nome}>Meu Game</h2>
-        <div>
-        <iframe 
-        className={styles.iframe}
-        width="410" 
-        height="315"
-        src="https://www.youtube.com/embed/6y6is0S8psw?autoplay=1&mute=1&controls=0" 
-        title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        allowfullscreen
-        >
-      </iframe>
-
       <iframe 
-      width="410" 
+      width="560" 
       height="315" 
       src="https://www.youtube.com/embed/xnmGIzGTWFU?autoplay=1&mute=1" 
       title="YouTube video player" 
@@ -27,7 +14,7 @@ function Game() {
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
       allowfullscreen
       >
-    </iframe></div>
+    </iframe>
     <div className={styles.tsd}>
       <a href="https://game-1-puce.vercel.app/" target="_blank" rel="noreferrer">
       <button className={styles.tsdgame}>Visitar site</button>
